52ac97c6e3c56cf1046fb79dd3be3fca615e3ee8
David Ho <homandat2002@gmail.com>
2023-04-21 14:15:10 -0500

    Messing around 4-21-23

--- David RL workspace/stablebaseline1.py
+++ David RL workspace/stablebaseline1.py
@@ -3,3 +3,20 @@ import gym
 from stable_baselines3 import ppo
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
+
+environment_name = 'CartPole-v0'
+env = gym.make(environment_name)
+
+episodes = 5
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
+    print('Episode:{} Score:{}'.format(episode, score))
+env.close()
